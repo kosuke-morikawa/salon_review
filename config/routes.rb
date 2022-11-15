0840f468e00302d9ff2f293114803b84dc925181
kosuke-morikawa <haoyousenchuan@gmail.com>
2022-11-15 22:54:56 +0900

    ルーティングの修正

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,6 @@
 Rails.application.routes.draw do
-  get 'beauty_shops/index'
-  get 'beauty_shops/show'
   devise_for :users
   root 'homes#top'
+  resources :beauty_shops, only: [:index, :show]
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
