ea826f1209f26e9597a6013ef8a0fe15ebedc700
kosuke-morikawa <haoyousenchuan@gmail.com>
2022-11-15 22:59:47 +0900

    コントローラのindex,showの記述

--- app/controllers/beauty_shops_controller.rb
+++ app/controllers/beauty_shops_controller.rb
@@ -1,7 +1,9 @@
 class BeautyShopsController < ApplicationController
   def index
+    @beauty_shops = BeautyShop.all
   end
 
   def show
+    @beauty_shop = BeautyShop.find(params[:id])
   end
 end
